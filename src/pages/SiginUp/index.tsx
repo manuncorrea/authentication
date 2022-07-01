417d4476bcb520fa351be5028e4ef605c7246b31
Emanuele Correa <manuncorrea@hotmail.com>
2022-07-01 11:04:00 -0300

    validando os dados do input

--- src/pages/SiginUp/index.tsx
+++ src/pages/SiginUp/index.tsx
@@ -1,7 +1,10 @@
 
-import { FiArrowLeft, FiLock, FiMail, FiUser } from 'react-icons/fi'
+import { useCallback } from 'react';
 import { Link } from 'react-router-dom';
 import { Form } from '@unform/web';
+import * as Yup from 'yup';
+
+import { FiArrowLeft, FiLock, FiMail, FiUser } from 'react-icons/fi'
 
 import { BoxContent } from "../../components/BoxContent";
 import { Button } from '../../components/Button';
@@ -10,9 +13,20 @@ import { Input } from "../../components/Input";
 import { Container } from './styles';
 
 export function SiginUp() {
-  function handleSubmit(data: object ) {
-    console.log(data)
-  }
+ const handleSubmit = useCallback( async (data: object ) => {
+    try {
+     const schema = Yup.object().shape({
+      name: Yup.string().required('Nome obrigatório'),
+      email: Yup.string().required('E-mail obrigatório').email('Digite um -email válido'),
+      password: Yup.string().min(6, 'No mínimo 6 dígitos')
+     });
+     await schema.validate(data, {
+      abortEarly: false, 
+     });
+    } catch (error) {
+      console.log(error)
+    }
+  }, []);
   return(
     <>
       <BoxContent>
